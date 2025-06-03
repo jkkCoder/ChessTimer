99da959a2562ef742b7036e5ec613e77e69516ad
ShunmugaSundaramK <shunmuga.sundaram@maghil.com>
2025-06-03 21:56:36 +0530

    Customer Slider added

--- src/screens/Timer/index.tsx
+++ src/screens/Timer/index.tsx
@@ -1,42 +1,51 @@
-import React, { useState } from 'react'
-import { View, Text, Pressable, SectionList } from 'react-native'
-import { NavigationProp, useNavigation } from '@react-navigation/native'
-import { chessTimeControls } from '../../constants/timerPillConstants';
-import { hp, wp } from '../../utils';
-import { styles } from './styles'
+import React, {useState} from 'react';
+import {View, Text, Pressable, SectionList} from 'react-native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {chessTimeControls} from '../../constants/timerPillConstants';
+import {hp, wp} from '../../utils';
+import {styles} from './styles';
 import TimerPill from '../../components/timerPill';
-import { ITimerPillValue } from 'interface/timerInterface';
-
-
+import {ITimerPillValue} from 'interface/timerInterface';
+import Slider from '@react-native-community/slider';
 
 const Timer: React.FC = () => {
-  const navigation = useNavigation<NavigationProp<any>>()
+  const navigation = useNavigation<NavigationProp<any>>();
 
-  const [selectedTimerPillValue, setSelectedTimerPillValue] = useState<ITimerPillValue | {}>({});
-
-  console.log({selectedTimerPillValue})
+  const [selectedTimerPillValue, setSelectedTimerPillValue] = useState<
+    ITimerPillValue | {}
+  >({});
+  const [initialTime, setSInitialTime] = useState(0);
+  const [bonusTime, setBonusTime] = useState(0);
 
   const handleGoBackToHomeCB = () => {
-    navigation.navigate('Home')
+    navigation.navigate('Home');
+  };
+
+  //For Custom timer
+  const handleGetValuesToParentCB = () => {
+    setSelectedTimerPillValue({
+      displayString: `${initialTime} | ${bonusTime}`,
+      incrementalValue: bonusTime,
+      time: initialTime
+    })
   }
 
   return (
     <View style={styles.timerScreenContainer}>
       <Pressable onPress={handleGoBackToHomeCB}>
         <Text>Go To Home</Text>
-      </Pressable >
+      </Pressable>
       <View style={styles.sectionTimerPillContainer}>
         <SectionList
           sections={chessTimeControls}
           keyExtractor={(item, index) => item?.displayString + index}
-          renderSectionHeader={({ section: { title, Icon: IconComponent } }) => (
+          renderSectionHeader={({section: {title, Icon: IconComponent}}) => (
             <View style={styles.sectionTimerHeaderContainer}>
-              <IconComponent width={wp(5)} height={hp(5)}/>
+              <IconComponent width={wp(5)} height={hp(5)} />
               <Text style={styles.sectionTimerHeaderText}>{title}</Text>
             </View>
           )}
-          renderItem={({ item, index, section }) => {
-
+          renderItem={({item, index, section}) => {
             if (index % 3 !== 0) return null;
 
             const items = section.data;
@@ -61,8 +70,57 @@ const Timer: React.FC = () => {
           }}
         />
       </View>
-    </View>
-  )
-}
 
-export default Timer
+      <View style={styles.sliderContainer}>
+        <Text style={styles.timeHeader}>
+          Initial Time <Text style={styles.time}>{initialTime} min</Text>
+        </Text>
+        <View style={styles.sliderWrapper}>
+          <Text style={styles.startTime}>0</Text>
+          <View style={styles.sliderparent}>
+            <Slider
+              style={styles.slider}
+              minimumValue={0}
+              maximumValue={120}
+              value={initialTime}
+              thumbTintColor="#D4AF37"
+              minimumTrackTintColor="#D4AF37"
+              maximumTrackTintColor="#B0B0B0"
+              onValueChange={value => setSInitialTime(Math.floor(value))}
+            />
+          </View>
+          <Text style={styles.endTime}>120</Text>
+        </View>
+
+        <Text style={styles.timeHeader}>
+          Bonus Time <Text style={styles.time}>{bonusTime} sec</Text>
+        </Text>
+        <View style={styles.sliderWrapper}>
+          <Text style={styles.startTime}>0</Text>
+          <View style={styles.sliderparent}>
+            <Slider
+              style={styles.slider}
+              minimumValue={0}
+              maximumValue={120}
+              value={bonusTime}
+              thumbTintColor="#D4AF37"
+              minimumTrackTintColor="#D4AF37"
+              maximumTrackTintColor="#B0B0B0"
+              onValueChange={value => setBonusTime(Math.floor(value))}
+            />
+          </View>
+          <Text style={styles.endTime}>60</Text>
+        </View>
+
+        <Pressable 
+          style={styles.selectBtn}
+          onPress={handleGetValuesToParentCB}
+        >
+          <Text style={styles.btnColor}>Select</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default Timer;
